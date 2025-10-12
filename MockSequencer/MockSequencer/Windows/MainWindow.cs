f128e60cc7854cb5b963f6d575d9768053683163
Karou <leaguehatesme@gmail.com>
2025-10-12 13:48:10 -0400

    .

--- MockSequencer/MockSequencer/Windows/MainWindow.cs
+++ MockSequencer/MockSequencer/Windows/MainWindow.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Numerics;
+
 namespace MockSequencer.Windows;
 
 using DalaMock.Shared.Interfaces;
@@ -10,9 +13,41 @@ using Dalamud.Bindings.ImGuizmo;
 public class MainWindow : Window
 {
     private readonly IFont font1;
-    
-    
 
+    public float[] Cameraview = {
+        1f, 0f, 0f, 0f,
+        0f, 1f, 0f, 0f,
+        0f, 0f, 1f, 0f,
+        0f, 0f, 0f, 1f
+    }; 
+    public float[] Cameraprojection = {
+        1f, 0f, 0f, 0f,
+        0f, 1f, 0f, 0f,
+        0f, 0f, 1f, 0f,
+        0f, 0f, 0f, 1f
+    }; 
+    
+    public float[][] objectMatrix ={
+        new float[] {1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1 },
+
+        new float[] {1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1 },
+
+        new float[] {1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1 },
+
+        new float[] {1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1 }
+    };
     public MainWindow(IFont font)
         : base("MockSequencer")
     {
@@ -20,12 +55,30 @@ public class MainWindow : Window
     }
 
     
-    public override void Draw()
+    public unsafe override void Draw()
     {
-        /*
-        ImViewGuizmo.ImViewGuizmo.
-        ImGuizmo.DrawCubes();
-        */
+
+        /*float* pCameraview = stackalloc float[16];
+        float* pCameraprojection = stackalloc float[16];
+        float* pobjectMatrix = stackalloc float[64];
+        
+        for(var i = 0; i < 16; i++)
+        {
+            pCameraprojection[i] = Cameraprojection[i];
+            pCameraview[i] = Cameraview[i];
+            for (var j = 0; j < 4; j++)
+            {
+                pobjectMatrix[i] = objectMatrix[j][i];
+            }
+        }*/
+
+        
+        //ImGuizmo.DrawCubes(pCameraview, pCameraprojection, pobjectMatrix, 4);
+        
+        //ImViewGuizmo.ImViewGuizmo.;
+
+        //ImGuizmo.DrawCubes()
+        
         
     }
     
